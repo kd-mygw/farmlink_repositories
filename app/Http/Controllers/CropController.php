ff15fb2a1d16b53578dc9049c1bfbf170f29a5f1
miyagawa <kd1318964@st.kobedenshi.ac.jp>
2024-12-02 10:51:42 +0900

    add cropcontrolelr

--- app/Http/Controllers/CropController.php
+++ app/Http/Controllers/CropController.php
@@ -18,7 +18,26 @@ class CropController extends Controller
     }
 
     // 新規農作物の登録フォームを表示
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
 
+            'product_name' => 'require|string|max:255',
+            'name' => 'require|string|max:255',
+            'cultivation_method' => 'require|string',
+            'fertilizer_info' => 'nullable|string|max:255',
+            'pesticide_info' => 'nulable|string|max:255',
+            'description' => 'require|string|max:255',
+            'cooking_tips' => 'require|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'video' => 'nullable|mimes:mp4,mov,ogg,qt|max:20000',
+
+        ]);
+        
+        
+
+    }
+    
     // 新規農作物の保存
 
     // 農作物の編集フォームを表示
