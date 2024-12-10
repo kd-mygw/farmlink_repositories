ad4318b66434913596529071a05bffa37f234f35
kazuma <kazumamiyoshi0311@gmail.com>
2024-12-10 10:28:40 +0900

    public.css適用

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,7 +7,7 @@
     <title>野菜情報公開ページ</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('/css/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/public.css')}}">
 
 </head>
 <body>
