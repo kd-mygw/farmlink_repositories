fd8efc2aaeec926c7beab5f32e503bf78ff55e05
kazuma <kazumamiyoshi0311@gmail.com>
2025-01-20 10:32:23 +0900

    icon追加

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -12,9 +12,7 @@
     <div class="ledger-grid">
         <div class="ledger-card">
             <a href="{{ route('record.harvest.index') }}">
-                <svg class="ledger-card-icon" viewBox="0 0 24 24">
-                    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5 14.5 7.62 14.5 9 13.38 11.5 12 11.5z"/>
-                </svg>
+                <img src="{{asset('/images/収穫アイコン.png')}}" alt="">
                 <span class="ledger-card-title">収穫</span>
             </a>
         </div>
